cdb0b6f90f2b8322270ab1ba9c90d7aa75a96003
Danmoreng <9203330+Danmoreng@users.noreply.github.com>
2025-08-17 20:20:36 +0200

    improve styling

--- tools/server/webui/src/lib/components/app/chat/ChatHTMLEditor/CodeEditor.svelte
+++ tools/server/webui/src/lib/components/app/chat/ChatHTMLEditor/CodeEditor.svelte
@@ -2,12 +2,27 @@
     import { editorStore } from "$lib/stores/editor.svelte.js";
 </script>
 
-<div class="flex flex-col flex-1 flex-grow bg-gray-600 pt-16">
+<div class="flex flex-col flex-1 h-full flex-grow bg-gray-600 pt-16">
     <h1>Header elements</h1>
     <div class="flex flex-row h-full">
-        <input class="flex-1 bg-gray-800 text-amber-50" bind:value={editorStore.currentCode} />
+        <textarea class="flex-1" bind:value={editorStore.currentCode}></textarea>
         <div class="flex flex-1 border-2 border-gray-300">
             <iframe sandbox="allow-scripts" srcdoc={editorStore.currentCode}></iframe>
         </div>
     </div>
 </div>
+
+<style>
+    textarea {
+        width: 100%;
+        padding: 10px;
+        background-color: #1e1e2f;
+        border: none;
+        font-family: 'Courier New', Courier, monospace;
+        font-size: 14px;
+        color: #f8f8f2;
+        border-radius: 4px;
+        resize: none;
+        min-height: 0;
+    }
+</style>
--- tools/server/webui/src/lib/stores/editor.svelte.ts
+++ tools/server/webui/src/lib/stores/editor.svelte.ts
@@ -1,5 +1,21 @@
 export class EditorStore {
-	currentCode = $state('<html lang="en"><body><h1>Hello World!</h1></body></html>');
+	currentCode = $state(`<html lang="en">
+	<style>
+		body {
+		  display: flex;
+		  justify-content: center;
+		  align-items: center;
+		  font-family: "Segoe UI", sans-serif;
+		  color: #fff;
+		  width: 100vw;
+		  height: 100vh;
+		  overflow: hidden;
+		}
+	</style>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>`);
 
 	setCode = (v: string) => (this.currentCode = v);
 	clearEditor = async () => {
